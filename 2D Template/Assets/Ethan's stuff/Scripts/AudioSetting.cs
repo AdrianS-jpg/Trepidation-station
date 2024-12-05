e1a17da3d29cf978312c8c7309f64b5401bce8cb
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2024-12-05 13:23:33 -0800

    Adding AudioSettings P.1

---
+++ 2D Template/Assets/Ethan's stuff/Scripts/AudioSetting.cs
@@ -0,0 +1,44 @@
+
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+
+    const string MIXER_MUSIC = "MusicVolume";
+    const string MIXER_SFX = "SFXVolume";
+
+    void Active()
+    {
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetMusicVolume);
+    }
+
+    void OnDisable()
+    {
+        PlayerPrefs.SetFloat();
+    }
+
+    void SetMusicVolume(float value)
+    {
+        mixer.SetFloat(MIXER_MUSIC, Mathf.Log10(value) * 20);
+    }
+    void SetSFXVolume(float value)
+    {
+        mixer.SetFloat(MIXER_MUSIC, Mathf.Log10(value) * 20);
+    }
+
+
+
+
+
+
+
+
+
+
+}
