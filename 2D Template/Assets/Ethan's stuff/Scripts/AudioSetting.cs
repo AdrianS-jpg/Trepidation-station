765f230a8f5c98ed9b496e75e1c9b85c0c515b08
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2024-12-06 11:22:18 -0800

    Fixed ed

--- 2D Template/Assets/Ethan's stuff/Scripts/AudioSetting.cs
+++ 2D Template/Assets/Ethan's stuff/Scripts/AudioSetting.cs
@@ -5,32 +5,32 @@ using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [SerializeField] AudioMixer mixer;
-    [SerializeField] Slider musicSlider;
-    [SerializeField] Slider sfxSlider;
+    //[SerializeField] AudioMixer mixer;
+    //[SerializeField] Slider musicSlider;
+    //[SerializeField] Slider sfxSlider;
 
-    const string MIXER_MUSIC = "MusicVolume";
-    const string MIXER_SFX = "SFXVolume";
+    //const string MIXER_MUSIC = "MusicVolume";
+    //const string MIXER_SFX = "SFXVolume";
 
-    void Active()
-    {
-        musicSlider.onValueChanged.AddListener(SetMusicVolume);
-        sfxSlider.onValueChanged.AddListener(SetMusicVolume);
-    }
+    //void Active()
+    //{
+    //    musicSlider.onValueChanged.AddListener(SetMusicVolume);
+    //    sfxSlider.onValueChanged.AddListener(SetMusicVolume);
+    //}
 
-    void OnDisable()
-    {
-        PlayerPrefs.SetFloat();
-    }
+    //void OnDisable()
+    //{
+    //    PlayerPrefs.SetFloat();
+    //}
 
-    void SetMusicVolume(float value)
-    {
-        mixer.SetFloat(MIXER_MUSIC, Mathf.Log10(value) * 20);
-    }
-    void SetSFXVolume(float value)
-    {
-        mixer.SetFloat(MIXER_MUSIC, Mathf.Log10(value) * 20);
-    }
+    //void SetMusicVolume(float value)
+    //{
+    //    mixer.SetFloat(MIXER_MUSIC, Mathf.Log10(value) * 20);
+    //}
+    //void SetSFXVolume(float value)
+    //{
+    //    mixer.SetFloat(MIXER_MUSIC, Mathf.Log10(value) * 20);
+    //}
 
 
 
