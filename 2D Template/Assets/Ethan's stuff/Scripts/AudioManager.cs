c927486fe488e58efe951c940221f59d7c716068
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2024-12-06 13:26:29 -0800

    Audio Script

--- 2D Template/Assets/Ethan's stuff/Scripts/AudioManager.cs
+++ 2D Template/Assets/Ethan's stuff/Scripts/AudioManager.cs
@@ -1,13 +1,21 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
    public static AudioManager instance;
 
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] AudioSource clickSource;
+    [SerializeField] List<AudioClip> clickClips = new List<AudioClip>();
+
+    public const string MUSIC_KEY = "musicVolume";
+    public const string SFX_KEY = "sfxVolume";
+
     private void Awake()
     {
-      if (instance)
+      if (instance == null)
       {
             instance = this;
 
@@ -17,5 +25,23 @@ public class AudioManager : MonoBehaviour
       {
             Destroy(gameObject);
       }
+        VolumeLoader();
     }
+
+    public void clickSFX()
+    {
+        AudioClip clip = clickClips[Random.Range(0, clickClips.Count)];
+
+        clickSource.PlayOneShot(clip);
+    }
+
+    void VolumeLoader() // Volume saved in AudioSetting.cs
+    {
+        float musicVolume = PlayerPrefs.GetFloat(MUSIC_KEY, 1f);
+        float sfxVolume = PlayerPrefs.GetFloat(SFX_KEY, 1f);
+
+        //mixer.SetFloat(AudioSettings.MIXER_MUSIC, Mathf.Log10(musicVolume) * 20);
+        //mixer.SetFloat(AudioSettings.MIXER_SFX, Mathf.Log10(sfxVolume) * 20);
+    }
+
 }
--- 2D Template/Assets/Ethan's stuff/Scripts/AudioSetting.cs
+++ 2D Template/Assets/Ethan's stuff/Scripts/AudioSetting.cs
@@ -9,18 +9,25 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] Slider musicSlider;
     [SerializeField] Slider sfxSlider;
 
-    const string MIXER_MUSIC = "MusicVolume";
-    const string MIXER_SFX = "SFXVolume";
+    public const string MIXER_MUSIC = "MusicVolume";
+    public const string MIXER_SFX = "SFXVolume";
 
-    void Active()
+    void Awake()
     {
         musicSlider.onValueChanged.AddListener(SetMusicVolume);
-        sfxSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    void Start()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat(AudioManager.MUSIC_KEY, 1f);
+        sfxSlider.value = PlayerPrefs.GetFloat(AudioManager.SFX_KEY, 1f);
     }
 
     void OnDisable()
     {
-        //PlayerPrefs.SetFloat();
+        PlayerPrefs.SetFloat(AudioManager.MUSIC_KEY, musicSlider.value);
+        PlayerPrefs.SetFloat(AudioManager.SFX_KEY, sfxSlider.value);
     }
 
     void SetMusicVolume(float value)
@@ -29,7 +36,7 @@ public class NewBehaviourScript : MonoBehaviour
     }
     void SetSFXVolume(float value)
     {
-        mixer.SetFloat(MIXER_MUSIC, Mathf.Log10(value) * 20);
+        mixer.SetFloat(MIXER_SFX, Mathf.Log10(value) * 20);
     }
 
 
