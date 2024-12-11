61264575c529436199ce7ffa4e11fa845041fe84
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2024-12-11 12:10:51 -0800

    Fullscreen0.1

---
+++ 2D Template/Assets/Ethan's stuff/Scripts/Fullscreen.cs
@@ -0,0 +1,16 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fullscreen : MonoBehaviour
+{
+   public void OnClickFullScreen()
+   {
+      
+
+
+
+   }
+}
