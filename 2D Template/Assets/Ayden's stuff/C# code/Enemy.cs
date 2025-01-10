134de94cd282a5b609671a6ec3cfb42a1d30cb25
Ayden-Kern <1722902@mukilteo.wednet.edu>
2025-01-10 13:11:25 -0800

    made the gun work better by making the enemy move around

--- 2D Template/Assets/Ayden's stuff/C# code/Enemy.cs
+++ 2D Template/Assets/Ayden's stuff/C# code/Enemy.cs
@@ -6,8 +6,17 @@ public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     public static Sprite monster;
+    public float speed;
+    
     private void Start()
     {
         GetComponent<SpriteRenderer>().sprite = monster;
+
+        float random = Random.Range(1, 360);
+        float x = Mathf.Sin(random);
+        float y = Mathf.Cos(random);
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(x, y)*speed;
+
     }
 }
