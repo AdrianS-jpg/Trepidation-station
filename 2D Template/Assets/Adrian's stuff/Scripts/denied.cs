0371a0dba01fd7bcec2905725290b2797aa5e638
aged_soup-admin <adrian.suharjoto@gmail.com>
2025-01-09 19:28:13 -0800

    hasdfghjk

---
+++ 2D Template/Assets/Adrian's stuff/Scripts/denied.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class denied : MonoBehaviour
+{
+    public bool active = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active == true){
+
+        } else 
+        {
+
+        }
+    }
+
+    public void whenPressedTwo()
+    {
+        if (active == false)
+        {
+            active = true;
+        } else {
+            active = false;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (active == false){
+            // if (other.collider.gameObject.name == hit.collider.gameObject) {
+
+            // }
+        }
+    }
+}
