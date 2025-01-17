28b9a057d60a4e5d07dc9c39295082d1b54fe5ca
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2025-01-17 13:11:54 -0800

    The timer for dialogue

--- 2D Template/Assets/Ethan's stuff/Scripts/DialogueUI.cs
+++ 2D Template/Assets/Ethan's stuff/Scripts/DialogueUI.cs
@@ -79,12 +79,12 @@ public class DialogueUI : MonoBehaviour
         {
             string dialogue = dialogueObject.Dialogue[i];
             yield return slowTalk.Run(dialogue, textLabel);
-            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));              // This is if we want it to be an input for the next dialogue instead of it ending and going to the next piece.
-            //yield return new WaitForSeconds(2);
+            /*yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));*/              // This is if we want it to be an input for the next dialogue instead of it ending and going to the next piece.
+            yield return new WaitForSeconds(3);
 
             if (i == dialogueObject.Dialogue.Length - 1 && dialogueObject.Responses != null && dialogueObject.HasResponses) break;
 
-            yield return new WaitUntil(() => Input.GetKeyUp(KeyCode.Space));
+            //yield return new WaitUntil(() => Input.GetKeyUp(KeyCode.Space));
         }
 
         if (dialogueObject.HasResponses)
